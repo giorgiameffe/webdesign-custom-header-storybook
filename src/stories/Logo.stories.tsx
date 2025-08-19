a584cc485ced4f796d1c519fcfd7105173ecfea7
giorgiameffe <giorgiameffe03@gmail.com>
2025-08-19 12:25:25 +0200

    feat: create stories for Logo component

---
+++ src/stories/Logo.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { Logo } from "../components/Logo/Logo";
+
+const meta: Meta<typeof Logo> = {
+    title: 'Atoms/Logo',
+    component: Logo,
+    parameters: {
+        layout: 'centered',
+        docs: {
+            description: {
+                component: 'Componente che visualizza il nome del brand in forma testuale, usato come logo.'
+            }
+        }
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        text: { control: 'text', description: 'Contenuto testuale visualizzato come logo principale.' }
+    }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: { text: 'My Brand' },
+    render: ({ text }) => <Logo text={text} />,
+};
