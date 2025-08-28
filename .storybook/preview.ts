c9b84fcfa554656a4821805e6fdd6a8f2bdd34a7
giorgiameffe <giorgiameffe03@gmail.com>
2025-08-28 10:50:45 +0200

    feat: add decorators

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,6 +9,9 @@ import "../src/styles/variables.css";
 // Importa il file di tipografia (font, pesi, dimensioni testo, ecc.)
 import "../src/styles/typography.css";
 
+// Importa il decoratore per supportare il cambio tema (light/dark)
+import { withThemeByClassName } from "@storybook/addon-themes";
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -25,6 +28,19 @@ const preview: Preview = {
       test: 'todo'
     }
   },
+
+
+  decorators: [
+    // Applica i temi "light" e "dark" aggiungendo una classe al <body>
+    withThemeByClassName({
+      themes: {
+        light: "light-theme",    // classe CSS per il tema chiaro
+        dark: "dark-theme",      // classe CSS per il tema scuro
+      },
+      defaultTheme: "light",     // tema predefinito
+      parentSelector: "body",    // il selettore su cui viene applicata la classe del tema
+    }),
+  ],
 };
 
 export default preview;
